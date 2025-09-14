307ada139a631c49e39192761928c315c4dd0d27
Yash <randomyashh@gmail.com>
2025-09-14 18:08:02 +0530

    Add src/app/action.ts

---
+++ src/app/action.ts
@@ -0,0 +1,8 @@
+"use server";
+import { neon } from "@neondatabase/serverless";
+
+export async function getData() {
+    const sql = neon(process.env.DATABASE_URL as string);
+    const data = await sql`...`;
+    return data;
+}
