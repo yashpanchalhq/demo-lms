0e5c00e204683a3e2fea438ce16907ceebb1d5a9
Yash <randomyashh@gmail.com>
2025-09-16 09:16:51 +0530

    feat: Update sign-up page component

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />; 
+  return (
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <SignUp />
+    </div>
+  ); 
 }
