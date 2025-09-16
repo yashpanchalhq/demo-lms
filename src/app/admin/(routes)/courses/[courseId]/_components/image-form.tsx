7bfb77dc240541411ef7bbad59ac1f449ad36e57
Yash <randomyashh@gmail.com>
2025-09-16 05:56:10 +0530

    feat: Update course image form component

--- src/app/admin/(routes)/courses/[courseId]/_components/image-form.tsx
+++ src/app/admin/(routes)/courses/[courseId]/_components/image-form.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
-import { Course } from "@prisma/client";
+import { Course } from "@/lib/teacher";
 import Image from "next/image";
 import { FileUpload } from "@/components/file-upload";
 
