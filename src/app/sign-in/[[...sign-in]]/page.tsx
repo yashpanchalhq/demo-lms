61cb9b3bfdcdf40d156c8d542105943ae9cbf540
Yash <randomyashh@gmail.com>
2025-09-16 09:16:44 +0530

    feat: Update sign-in page component

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />; 
+  return (
+    <div className="min-h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <SignIn />
+    </div>
+  ); 
 }
