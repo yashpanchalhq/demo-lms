557a6824c6f45b93fe066dc75fbf4bf5b8f4057f
Yash <randomyashh@gmail.com>
2025-09-16 06:00:19 +0530

    feat: Update enroll route

--- src/app/api/enroll/route.ts
+++ src/app/api/enroll/route.ts
@@ -1,8 +1,8 @@
 import { getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getSupabaseClient } from "@/lib/supabase";
 
-export async function POST(req: Request) {
+export async function POST(req: NextRequest) {
   try {
     const { userId: clerkUserId } = getAuth(req);
 
