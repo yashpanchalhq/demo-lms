0c70e837a7b91dfe5196d03b26a109e2536abe73
Yash <randomyashh@gmail.com>
2025-09-16 06:00:07 +0530

    feat: Update course route

--- src/app/api/courses/[courseId]/route.ts
+++ src/app/api/courses/[courseId]/route.ts
@@ -1,15 +1,15 @@
 
 import { getAuth } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { getSupabaseClient } from "@/lib/supabase";
 
 export async function PATCH(
-  req: Request,
-  { params }: { params: { courseId: string } }
+  req: NextRequest,
+  context: any
 ) {
   try {
     const { userId } = getAuth(req);
-    const { courseId } = params;
+    const { courseId } = context.params;
     const values = await req.json();
 
     if (!userId) {
@@ -38,12 +38,12 @@ export async function PATCH(
 }
 
 export async function DELETE(
-  req: Request,
-  { params }: { params: { courseId: string } }
+  req: NextRequest,
+  context: any
 ) {
   try {
     const { userId } = getAuth(req);
-    const { courseId } = params;
+    const { courseId } = context.params;
 
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
